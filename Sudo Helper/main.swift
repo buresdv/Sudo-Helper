48e5e51e52888a2b00902cc71746a981c4d53f1f
David Bureš <buresdv@gmail.com>
2024-06-01 23:13:15 +0200

    + First implementation of dialog

--- Sudo Helper/main.swift
+++ Sudo Helper/main.swift
@@ -5,7 +5,42 @@
 //  Created by David Bureš on 01.06.2024.
 //
 
+import Cocoa
 import Foundation
 
-print("Hello, World!")
+let application = NSApplication.shared
+NSApp.setActivationPolicy(.accessory)
+NSApp.activate(ignoringOtherApps: true)
 
+let alert = NSAlert()
+
+alert.window.title = "Cork Password Helper"
+alert.messageText = "Homebrew action requires elevated privileges"
+alert.informativeText = "Input your administrator password to let Cork perform this privileged task on your behalf"
+
+alert.icon = NSImage(named: "NSSecurity")
+
+alert.alertStyle = .informational
+
+let submitButton = alert.addButton(withTitle: "Submit")
+submitButton.tag = NSApplication.ModalResponse.OK.rawValue
+
+let cancelButton = alert.addButton(withTitle: "Cancel")
+cancelButton.tag = NSApplication.ModalResponse.cancel.rawValue
+
+let passwordField = NSSecureTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 24))
+
+alert.accessoryView = passwordField
+alert.window.initialFirstResponder = passwordField
+
+let response = alert.runModal()
+
+switch response
+{
+case .OK:
+    print(passwordField.stringValue)
+    exit(0)
+default:
+    print("")
+    exit(-1)
+}
